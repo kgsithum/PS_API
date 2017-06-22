f0daf619ee8c7e191269bb8ccfd14541e7acc8c5
Sithum <kgsithum@gmail.com>
2017-06-22 15:04:03 +1200

    Update article action

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -76,19 +76,78 @@ class ArticleController extends FOSRestController
         {
             return new View("Please enter URL.", Response::HTTP_NOT_ACCEPTABLE); 
         }
-        $data->setTitle($title);
-        $data->setAuthorId($title);
+        $data->setAuthorId($author_id);
+        $data->setTitle($title);        
         $data->setUrl($url);
         $data->setContent($content);
 
-        $currenttime = date("Y-m-d H:i:s");
+        $currenttime = new \DateTime();
         $data->setCreatedAt($currenttime);
         $data->setUpdatedAt($currenttime);
         
         $em = $this->getDoctrine()->getManager();
         $em->persist($data);
         
+        $em->flush();
+        
         $view = $this->view("Article Added Successfully", Response::HTTP_OK);
+        return $view;
+	}
+
+
+    /**
+	 * @Rest\Put("/article/{id}")
+	 */
+	public function updateAction($id,Request $request)
+	{
+        
+        $author_id = $request->get('author_id');
+        $title = $request->get('title');
+        $url = $request->get('url');
+        $content = $request->get('content');
+        
+        
+        //validations
+
+        //check for valid article
+        $article = $this->getDoctrine()->getRepository('AppBundle:Article')->find($id);
+        if($article === null){
+            return new View("Please provide valid article.", Response::HTTP_NOT_ACCEPTABLE); 
+        }
+        if($author_id !== null)
+        {
+            //check valid author id
+            $validAuthor = $this->getDoctrine()->getRepository('AppBundle:Author')->find($author_id);
+            if ($validAuthor === null) {
+                return new View("Please enter valid Author ID.", Response::HTTP_NOT_ACCEPTABLE);
+            }else{
+                $article->setAuthorId($author_id);
+            }
+        }
+
+        if($title !== null)
+        {
+            $article->setTitle($title);  
+        }
+        if($url !== null)
+        {
+            $article->setUrl($url);
+        }
+        if($content !== null)
+        {
+            $article->setContent($content);
+        }
+        
+
+        $currenttime = new \DateTime();        
+        $article->setUpdatedAt($currenttime);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        
+        $em->flush();
+        
+        $view = $this->view("Article Updated Successfully", Response::HTTP_OK);
         return $view;
 	}
 	
